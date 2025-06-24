98d718733437a6745ee238f79da231b82bc41bf6
Aryan Dagur <kartik79907@gmail.com>
2025-06-24 23:33:33 +0530

    ex

--- draft/script.js
+++ draft/script.js
@@ -1,2 +1,32 @@
-alert("Hello World");
-console.log("Code is running...");
+function adj()
+{
+    let a="Crazy";
+    let b="Amazing";
+    let c="Fire";
+    let d=Math.trunc(Math.random()*10)%3;
+    if(d==0){return a;}
+    else if(d==1){return b;}
+    else {return c;}
+
+}
+function sn()
+{
+    let a="Engine";
+    let b="Foods";
+    let c="Garments";
+    let d=Math.trunc(Math.random()*10)%3;
+    if(d==0){return a;}
+    else if(d==1){return b;}
+    else {return c;}
+}
+function aw()
+{
+    let a="Bros";
+    let b="Limited";
+    let c="Hub";
+    let d=Math.trunc(Math.random()*10)%3;
+    if(d==0){return a;}
+    else if(d==1){return b;}
+    else {return c;}
+}
+console.log(adj()+sn()+aw());
