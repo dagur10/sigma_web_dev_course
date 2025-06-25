08b742a278ecf8e9cbf192cfb9613bdcb69230dd
Aryan Dagur <kartik79907@gmail.com>
2025-06-26 03:15:30 +0530

    ex13

--- draft/new.js
+++ draft/new.js
@@ -1,5 +1,7 @@
-let a=1;
-let b=5.55;
-let c="string";
-console.log(a,b,c);
-console.log(typeof(a),typeof(b),typeof(c));
+let cont = document.getElementsByClassName("box");
+Array.from(cont).forEach(element => {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    element.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+});
