03ec99b02b8feabb167698b76eeabe61222a17b3
Aryan Dagur <kartik79907@gmail.com>
2025-06-27 00:06:13 +0530

    ex13

--- draft/new.js
+++ draft/new.js
@@ -1,7 +1,17 @@
-let cont = document.getElementsByClassName("box");
-Array.from(cont).forEach(element => {
-    let r = Math.floor(Math.random() * 256);
-    let g = Math.floor(Math.random() * 256);
-    let b = Math.floor(Math.random() * 256);
-    element.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-});
+function createCard(title, cName, views, monthsOld, duration, thumbnail) {
+   let html=`<div class="image">
+            <img src=${thumbnail}>
+            <div class="dur">
+                ${duration}
+            </div>
+        </div>
+        <div class="content">
+            <h2>${title}</h2>
+            <p>${cName} . ${views} . ${monthsOld}</p>
+        </div>`;
+    document.querySelector(".container").innerHTML=document.querySelector(".container").innerHTML+html;   
+    
+}
+
+
+createCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithHarry", 560000, 7, "31:22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw")
